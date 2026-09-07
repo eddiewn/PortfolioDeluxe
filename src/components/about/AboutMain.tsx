dbf12cf0cc4b1080675a9edde6ed1114f408f102
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2026-09-07 10:17:58 +0200

    Fixed responsiveness

--- src/components/about/AboutMain.tsx
+++ src/components/about/AboutMain.tsx
@@ -3,11 +3,11 @@ import profilePic from "../../assets/images/profilePic.webp"
 function AboutMain(){
     return(
         <>
-            <main className="flex flex-row justify-between w-4/5 m-auto">
-                <section className="w-4/10">
+            <main className="flex flex-col md:flex-row gap-5 md:gap-0 justify-between w-4/5 m-auto">
+                <section className="md:w-4/10">
                     <img src={profilePic} alt="" />
                 </section>
-                <section className="w-4/10 flex flex-col gap-3">
+                <section className="md:w-4/10 flex flex-col gap-5 md:gap-3 text-2xl md:text-base">
                     <h1 className="text-4xl font-extrabold">Hello you! I'm a Frontend Webdeveloper from Sweden :]</h1>
                     <p>I started my programming journey not like many others, with accidentally choosing a programming "Gymnasie" instead of general IT, OOPS! It is not anything I regret as I found it very fun to work with.</p>
                     <p>My favorite tools to work with is React and TailwindCSS as I love the clean feeling of having everything in one file!</p>
